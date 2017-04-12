22a6b850121b5bc34a42a7097fe711dabc9d0e4d
Zach Hirschtritt <Zach Hirschtritt>
2017-04-12 14:34:38 -0400

    adds button state check

--- tinketGamePad/tinketGamePad.ino
+++ tinketGamePad/tinketGamePad.ino
@@ -1,24 +1,25 @@
 #include <ProTrinketKeyboard.h>
 int pins[] = {0, 1, 3, 4, 5, 6};
-bool pinStates[] = {false, false, false, false , false, false};
+char prevStates[] = {HIGH, HIGH, HIGH, HIGH , HIGH, HIGH};
 int keycodes[] = {KEYCODE_SPACE, KEYCODE_ARROW_UP, KEYCODE_ARROW_DOWN, KEYCODE_ARROW_RIGHT, KEYCODE_ARROW_LEFT};
-
-long nextPress;
 
 void setup() {
   for (byte i = 0; i < sizeof(pins); i++) {
     pinMode(i, INPUT_PULLUP);
   }
-  nextPress = millis();
   TrinketKeyboard.begin();
 }
 
 void loop() {
   TrinketKeyboard.poll();
   for (byte i = 0; i < sizeof(pins); i++) {
-    if (digitalRead(pins[i]) == LOW) {
-      TrinketKeyboard.pressKey(0, keycodes[i]);
-      TrinketKeyboard.pressKey(0, 0);
+    if (prevSates[i] != digitalRead(pins[i])) {
+      if (digitalRead(pins[i]) == LOW) {
+        TrinketKeyboard.pressKey(0, keycodes[i]);
+      }
+      else {
+        TrinketKeyboard.pressKey(0, 0);
+      }
     }
   }
 }
